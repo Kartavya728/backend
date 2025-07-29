3a1e75df89d3571065c912909a7daaabae255f09
Kartavya728 <kartavya.xenon@gmail.com>
2025-07-29 18:33:14 +0530

    J

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -98,7 +98,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 ALLOWED_HOSTS = [
-    'kartavya728.pythonanywhere.com', 
+    'kartavya28.pythonanywhere.com', 
     '127.0.0.1',
     'localhost',
 ]
